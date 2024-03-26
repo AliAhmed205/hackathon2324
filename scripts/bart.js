80d2812248b37af08b6eba89f26437b63a9b0ab2
TristanBrattinga <tristan.brattinga@gmail.com>
2024-03-26 16:05:51 +0100

    fix random generate and countries

--- scripts/bart.js
+++ scripts/bart.js
@@ -1,178 +1,97 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let allData = []; // Store all the fetched data 
-    let yearColors = {};
+document.addEventListener('DOMContentLoaded', function() {
+    let allData    = [] // Store all the fetched data
+    let yearColors = {}
 
     // Fetch data from the JSON link
     fetch('https://cssday.nl/data/speakers.json')
         .then(response => response.json())
         .then(data => {
-            allData = data; // Store fetched data
-            displaySpeakers(data); // Display all speakers
-            updateCountryOptions(data); // Update the country options
-            updateYearColors(data); // Update the year colors
-            updateYearOptions(data);
-
+            allData = data // Store fetched data
+            updateYearColors(data) // Update the year colors
         })
         .catch(error => {
-            console.error('Error fetching data:', error); // Show the error in the console
-        });
+            console.error('Error fetching data:', error) // Show the error in the console
+        })
 
-    function updateYearOptions(data) {
-        const yearSelect = document.getElementById('year-select');
-        const allYears = data.map(speaker => speaker.edition.year)
-            .filter((year, index, self) => year && self.indexOf(year) === index)
-            .sort((a, b) => a - b); // Sorteer jaren in oplopende volgorde
-
-        yearSelect.innerHTML = '<option value="">All</option>'; // Reset en voeg de 'All' optie toe
-
-        allYears.forEach(year => {
-            const option = document.createElement('option');
-            option.value = option.textContent = year;
-            yearSelect.appendChild(option);
-        });
+    // FUNCTION TO RANDOMIZE ITEM FROM A GIVEN ARRAY
+    const getRandomItem = (array) => {
+        return array[Math.floor(Math.random() * array.length)]
     }
 
+    document.getElementById('randomButton').addEventListener('click', () => {
 
+        const randomYear = getRandomItem(Object.keys(yearColors))
+        console.log(randomYear)
+
+        const speakersForYear    = allData.filter(speaker => speaker.edition && speaker.edition.year === parseInt(randomYear))
+        const availableCountries = [...new Set(speakersForYear.map(speaker => speaker.country))]
+
+        const randomCountry             = getRandomItem(availableCountries)
+        const speakersForYearAndCountry = speakersForYear.filter(speaker => speaker.country === randomCountry)
+
+
+        displaySpeakers(speakersForYear)
+
+    })
+
+    // !TODO EXCLUDE 2024 SPEAKERS
 
     function displaySpeakers(data) {
-        const speakersContainer = document.getElementById('speakers-container');
-        speakersContainer.innerHTML = ''; // Clear previous data
+        const speakersContainer     = document.getElementById('speakers-container')
+        speakersContainer.innerHTML = '' // Clear previous data
 
         // Check if there are speakers to display
         if (data.length === 0) {
-            speakersContainer.textContent = 'No speakers found for the selected year.';
-            return;
+            speakersContainer.textContent = 'No speakers found for the selected year.'
+            return
         }
 
         // Create a list to display speakers
-        const speakersList = document.createElement('ul');
+        const speakersList = document.createElement('ul')
         data.forEach(speaker => {
-            const speakerItem = document.createElement('li');
 
-            // const svgMarkup = `<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 233.16 262.3">
-            // <defs>
-            // <style>
-            //       .cls-1 {
-            //         fill: #265aa6;
-            //         stroke-width: 0px;
-            //       }
-            // </style>
-            // </defs>
-            // <polygon class="cls-1" points="211.3 236.8 116.58 262.3 21.86 236.8 0 0 233.16 0 211.3 236.8"/>
-            // </svg>`;
+            if (speaker.avatar) {
+                const speakerItem = document.createElement('li')
 
-            // speakerItem.innerHTML = svgMarkup;
+                let speakerName         = document.createElement('h2')
+                speakerName.textContent = speaker.name
 
+                // Create an avatar image
+                const avatarImage = document.createElement('img')
+                // image of the speaker
+                avatarImage.src   = speaker.avatar
+                // alt text
+                avatarImage.alt   = speaker.name
 
-            let speakerName = document.createElement('h2');
-            speakerName.textContent = speaker.name;
+                const speakerTitle       = document.createElement('h3')
+                speakerTitle.textContent = speaker.talk.title
 
+                // create description
+                const speakerDescription = document.createElement('p')
 
-            // Create an avatar image
-            const avatarImage = document.createElement('img');
-            // image of the speaker
-            avatarImage.src = speaker.avatar;
-            // alt text
-            avatarImage.alt = speaker.name;
-
-
-            const speakerTitle = document.createElement('h3');
-            speakerTitle.textContent = speaker.talk.title;
-
-
-            // create description
-            const speakerDescription = document.createElement('p');
-            speakerDescription.textContent = speaker.talk.description;
-
-
-            // showen
-
-
-            speakersList.appendChild(speakerItem);
-
-            speakerItem.appendChild(speakerName);
-
-            speakerItem.appendChild(avatarImage);
-
-            speakerItem.appendChild(speakerTitle);
-
-            speakerItem.appendChild(speakerDescription);
-
-
-        });
-
-        speakersContainer.appendChild(speakersList);
-    }
-
-    // dynamic years
-    function updateCountryOptions(data, selectedYear) {
-        const countrySelect = document.getElementById('country-select');
-        let filteredData = data;
-
-        // Als er een jaar geselecteerd is, filter dan de data op basis van dat jaar
-        if (selectedYear) {
-            filteredData = data.filter(speaker => speaker.edition && speaker.edition.year.toString() === selectedYear);
-        }
-
-        const allCountries = filteredData.map(speaker => speaker.country)
-            .filter((value, index, self) => self.indexOf(value) === index)
-            .sort();
-
-        countrySelect.innerHTML = '<option value="">All</option>'; // Reset en voeg de 'All' optie toe
-
-        allCountries.forEach(country => {
-            const option = document.createElement('option');
-            option.value = option.textContent = country;
-            countrySelect.appendChild(option);
-        });
+                if (speaker.day === false) {
+                    speakerDescription.textContent = 'Upcoming'
+                } else if (speaker.talk.description === false) {
+                    speakerDescription.textContent = ''
+                } else {
+                    speakerDescription.textContent = speaker.talk.description
+                }
+                speakersList.appendChild(speakerItem)
+                speakerItem.appendChild(speakerName)
+                speakerItem.appendChild(avatarImage)
+                speakerItem.appendChild(speakerTitle)
+                speakerItem.appendChild(speakerDescription)
+            }
+        })
+        speakersContainer.appendChild(speakersList)
     }
 
     function updateYearColors(data) {
         yearColors = data.reduce((acc, speaker) => {
             if (speaker.edition && speaker.edition.year && speaker.edition.color && speaker.edition.color.hex) {
-                acc[speaker.edition.year] = speaker.edition.color.hex;
+                acc[speaker.edition.year] = speaker.edition.color.hex
             }
-            return acc;
-        }, {});
+            return acc
+        }, {})
     }
-
-
-    document.getElementById('year-select').addEventListener('change', () => {
-        const selectedYear = document.getElementById('year-select').value;
-        const fill = yearColors[selectedYear];
-        document.querySelectorAll('svg').forEach(svg => {
-            svg.style.fill = fill;
-        });
-
-        updateCountryOptions(allData, selectedYear);
-        document.getElementById('country-select').value = '';
-
-    });
-
-
-
-    document.getElementById('filter-button').addEventListener('click', () => {
-        const selectedYear = document.getElementById('year-select').value;
-        const selectedCountry = document.getElementById('country-select').value;
-
-        const filteredSpeakers = allData.filter(speaker => {
-            // Check if the speaker matches the selected year and country 
-            // If no year or country is selected, all speakers should be displayed
-            // If a year or country is selected, only speakers that match the selected year and/or country should be displayed
-
-            // Deze regel controleert of de spreker overeenkomt met het geselecteerde jaar. 
-            // Als selectedYear leeg is (wat betekent dat de gebruiker 'All' of niets heeft geselecteerd), 
-            // of als het jaar van de spreker (speaker.edition.year) overeenkomt met selectedYear, 
-            // dan wordt matchesYear true. Anders is het false.
-            const matchesYear = !selectedYear || (speaker.edition && speaker.edition.year.toString() === selectedYear);
-
-            // If a country is selected, only speakers that match the selected country should be displayed
-            const matchesCountry = !selectedCountry || speaker.country === selectedCountry;
-            // Return true if the speaker matches the selected year and country
-            return matchesYear && matchesCountry;
-        });
-
-        displaySpeakers(filteredSpeakers);
-    });
-
-});
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,57 +1,57 @@
-const randomButton     = document.getElementById('randomButton')
-const countryContainer = document.getElementById('countrySlot')
-const viewsContainer   = document.getElementById('viewSlot')
-const yearsContainer   = document.getElementById('yearSlot')
-
-const countries = ['NL', 'EN', 'DE', 'US', 'UK', 'FR', 'NO', 'SE', 'CA', 'JP', 'BE', 'AT', 'PT']
-
-const views = ['5777', '2345', '4821', '3999', '5032', '4200', '5566', '4011', '5863', '4689', '5023', '5298', '4599']
-
-const years = ['2013', '2014', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
-
-const fillSlots = () => {
-    createList(countries, countryContainer)
-    createList(views, viewsContainer)
-    createList(years, yearsContainer)
-}
-
-const createList = (array, container) => {
-    let list = document.createElement('ul')
-
-    array.forEach((item) => {
-        let li         = document.createElement('li')
-        li.textContent = item
-        list.appendChild(li)
-    })
-
-    container.appendChild(list)
-}
-
-fillSlots()
-
-const getRandomIndex = (max) => {
-    return Math.floor(Math.random() * max)
-}
-
-randomButton.addEventListener('click', () => {
-    const countryIndex = getRandomIndex(countries.length)
-    const viewIndex    = getRandomIndex(views.length)
-    const yearIndex    = getRandomIndex(years.length)
-
-    updateSlots(countryContainer, countryIndex)
-    updateSlots(viewsContainer, viewIndex)
-    updateSlots(yearsContainer, yearIndex)
-})
-
-const updateSlots = (container, index) => {
-    const ul  = container.querySelector('ul')
-    const lis = ul.querySelectorAll('li')
-
-    lis.forEach((li, i) => {
-        if (i === index) {
-            li.style.display = 'block'
-        } else {
-            li.style.display = 'none'
-        }
-    })
-}
+// const randomButton     = document.getElementById('randomButton')
+// const countryContainer = document.getElementById('countrySlot')
+// const viewsContainer   = document.getElementById('viewSlot')
+// const yearsContainer   = document.getElementById('yearSlot')
+//
+// const countries = ['NL', 'EN', 'DE', 'US', 'UK', 'FR', 'NO', 'SE', 'CA', 'JP', 'BE', 'AT', 'PT']
+//
+// const views = ['5777', '2345', '4821', '3999', '5032', '4200', '5566', '4011', '5863', '4689', '5023', '5298', '4599']
+//
+// const years = ['2013', '2014', '2014', '2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024']
+//
+// const fillSlots = () => {
+//     createList(countries, countryContainer)
+//     createList(views, viewsContainer)
+//     createList(years, yearsContainer)
+// }
+//
+// const createList = (array, container) => {
+//     let list = document.createElement('ul')
+//
+//     array.forEach((item) => {
+//         let li         = document.createElement('li')
+//         li.textContent = item
+//         list.appendChild(li)
+//     })
+//
+//     container.appendChild(list)
+// }
+//
+// fillSlots()
+//
+// const getRandomIndex = (max) => {
+//     return Math.floor(Math.random() * max)
+// }
+//
+// randomButton.addEventListener('click', () => {
+//     const countryIndex = getRandomIndex(countries.length)
+//     const viewIndex    = getRandomIndex(views.length)
+//     const yearIndex    = getRandomIndex(years.length)
+//
+//     updateSlots(countryContainer, countryIndex)
+//     updateSlots(viewsContainer, viewIndex)
+//     updateSlots(yearsContainer, yearIndex)
+// })
+//
+// const updateSlots = (container, index) => {
+//     const ul  = container.querySelector('ul')
+//     const lis = ul.querySelectorAll('li')
+//
+//     lis.forEach((li, i) => {
+//         if (i === index) {
+//             li.style.display = 'block'
+//         } else {
+//             li.style.display = 'none'
+//         }
+//     })
+// }
