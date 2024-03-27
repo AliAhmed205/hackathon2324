79bfa13b2331880ec7f1d0d14d4da43284d89775
TristanBrattinga <tristan.brattinga@gmail.com>
2024-03-27 15:45:10 +0100

    fixed random countries and years slots

---
+++ newscript.js
@@ -0,0 +1,166 @@
+const countrySlot = document.getElementById('countrySlot')
+const yearSlot    = document.getElementById('yearSlot')
+let allData
+let countries
+let years
+
+// FETCH DATA
+fetch('https://cssday.nl/data/speakers.json')
+    .then(response => response.json())
+    .then(data => {
+        allData = data
+        getCountriesAndYears(data)
+    })
+    .catch(error => {
+        console.error('Error fetching data:', error)
+    })
+
+// MAKE ARRAYS FROM THE FETCHED DATA
+const getCountriesAndYears = (data) => {
+    countries = [...new Set(data.filter(item => item).map(item => item.country))]
+    years     = [...new Set(data.filter(item => item).map(item => item.edition.year))]
+
+    fillSlots(countries, years)
+}
+
+// FILL THE SLOTS WITH THE DATA
+const fillSlots = (countries, years) => {
+    createList(countries, countrySlot)
+    createList(years, yearSlot)
+}
+
+// CREATE LISTS FROM ARRAYS
+const createList = (array, container) => {
+    let list = document.createElement('ul')
+
+    array.forEach((item) => {
+        let li         = document.createElement('li')
+        li.textContent = item
+        list.appendChild(li)
+    })
+
+    container.appendChild(list)
+}
+
+const updateList = (array, container) => {
+    container.innerHTML = ''
+    let list            = document.createElement('ul')
+
+    array.forEach((item) => {
+        let li = document.createElement('li')
+
+        li.textContent = item
+
+        list.appendChild(li)
+    })
+
+    container.appendChild(list)
+}
+
+// FUNCTION TO RANDOMIZE ITEM IN A GIVEN ARRAY
+const getRandomItem = (array) => {
+    let index = Math.floor(Math.random() * array.length)
+    let value = array[index]
+
+    return { value, index }
+}
+
+// LEVER FUNCTIONALITY FOR SPINNING
+const lever = document.getElementById('lever')
+
+lever.addEventListener('click', () => {
+
+    lever.classList.add('spinning')
+    setTimeout(() => {
+        lever.classList.remove('spinning')
+    }, 800)
+
+    const randomYear = getRandomItem(years)
+
+    const speakersForYear = allData.filter(speaker => speaker.edition && speaker.edition.year === parseInt(randomYear.value))
+
+    const availableCountries = [...new Set(speakersForYear.map(speaker => speaker.country))]
+
+    updateList(availableCountries, countrySlot)
+
+    const randomCountry             = getRandomItem(availableCountries)
+    const speakersForYearAndCountry = speakersForYear.filter(speaker => speaker.country === randomCountry.value)
+
+    spinSlot(yearSlot, randomYear.index)
+    spinSlot(countrySlot, randomCountry.index)
+    displaySpeakers(speakersForYearAndCountry)
+
+    document.body.setAttribute('style', 'height: 100%; margin-top: 5rem; margin-bottom: 10rem;')
+    // document.documentElement.style.setProperty('--svg-color', yearColors[randomYear])
+})
+
+// FUNCTION TO SPIN THE SLOTS IN THE WHEEL
+const spinSlot = (elementId, index) => {
+    const ul = elementId
+
+    ul.style.setProperty('--aantal', 0);
+
+    setTimeout(() => {
+        ul.style.setProperty('--dur', 1);
+        ul.style.setProperty('--aantal', index);
+
+        ul.addEventListener('transitionend', () => {
+            ul.style.setProperty('--dur', 0);
+        });
+
+    }, 1);
+}
+
+// FUNCTION TO DISPLAY THE CORRECT SPEAKERS
+const displaySpeakers = (data) => {
+    const speakersContainer     = document.getElementById('speakers-container')
+    speakersContainer.innerHTML = ''
+
+    if (data.length === 0) {
+        speakersContainer.textContent = 'No speakers found for the selected year.'
+        return
+    }
+
+    const speakersList = document.createElement('ul')
+    data.forEach(speaker => {
+
+        if (speaker.avatar) {
+            const speakerItem = document.createElement('li')
+
+            let speakerYear         = document.createElement('p')
+            speakerYear.textContent = speaker.edition.date
+
+            let speakerCountry         = document.createElement('p')
+            speakerCountry.textContent = speaker.country
+
+            let speakerName         = document.createElement('h2')
+            speakerName.textContent = speaker.name
+
+            const avatarImage = document.createElement('img')
+            avatarImage.src   = speaker.avatar
+            avatarImage.alt   = speaker.name
+
+            const speakerTitle       = document.createElement('h3')
+            speakerTitle.textContent = speaker.talk.title
+
+            const speakerDescription = document.createElement('p')
+
+            if (speaker.day === false) {
+                speakerDescription.textContent = 'Upcoming'
+            } else if (speaker.talk.description === false) {
+                speakerDescription.textContent = ''
+            } else {
+                speakerDescription.textContent = speaker.talk.description
+            }
+
+            speakersList.appendChild(speakerItem)
+            speakerItem.appendChild(speakerCountry)
+            speakerItem.appendChild(speakerYear)
+            speakerItem.appendChild(speakerName)
+            speakerItem.appendChild(avatarImage)
+            speakerItem.appendChild(speakerTitle)
+            speakerItem.appendChild(speakerDescription)
+        }
+    })
+    speakersContainer.appendChild(speakersList)
+}
